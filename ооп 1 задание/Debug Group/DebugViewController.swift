e98b123e6b96faa8cb176b87a6af469c3c80deeb
ruzal <rusad21@gmail.com>
2021-09-13 03:13:06 +0300

    добавлен переход

---
+++ ооп 1 задание/Debug Group/DebugViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DebugMenu.swift
+//  ооп 1 задание
+//
+//  Created by Ruzal  on 13.09.2021.
+//
+
+
+import UIKit
+
+class DebugViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        // Do any additional setup after loading the view.    }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
